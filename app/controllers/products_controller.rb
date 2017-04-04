f7c4fb05b6448ae2b2f28f54bb7a2e6006ebb155
vsonnathi <sonnathivinil@gmail.com>
2017-04-04 15:35:02 -0400

    add action ,routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,10 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def step2
+    binding.pry
+  end
+
   # GET /products/1
   # GET /products/1.json
   def show
