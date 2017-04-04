a5f5a1f77fbb831fa196466d67170277ef84772a
vsonnathi <sonnathivinil@gmail.com>
2017-04-04 17:05:46 -0400

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Product.new(name: "pqr", description: "pzy", price: 26.21).save
+Product.new(name: "xyz", description: "xyz", price: 43.38).save
+Product.new(name: "ppp", description: "ppp", price: 23.26).save
+Product.new(name: "aaa", description: "aaa", price: 45.23).save
