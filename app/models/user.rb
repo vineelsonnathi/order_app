253fac5df6d528e5d17d5d310abeb9494b9d599d
vsonnathi <sonnathivinil@gmail.com>
2017-04-04 20:28:20 -0400

    show address and user info on step 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
 
   has_one :address
   accepts_nested_attributes_for :address
+
+  def full_name
+    first_name + ", " + middle_initial + ", " + last_name
+  end
+
+  def home_address
+    address.street_name_1 + ", " + address.street_name_2 + ", " + address.city + ", " + address.zip + ", " + address.zip_plus_four
+  end
 end
