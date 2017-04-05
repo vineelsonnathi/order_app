e082e2afe55f850ed2eb307c2104b14313d330f0
vsonnathi <sonnathivinil@gmail.com>
2017-04-04 20:45:58 -0400

    show product, edit/destroy links and redirect to step1 if refresh on step3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  rescue_from ::ActiveRecord::RecordNotFound, with: :record_not_found
+
+  protected
+
+  def record_not_found
+    session[:total_amount] = nil
+    redirect_to step1_products_path
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -68,7 +68,7 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to step1_products_path, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
